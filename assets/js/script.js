31bc1397676bf50101cf03aaf7e743e9ac577be9
Mountian Driver <lsfreigenberg@outlook.com>
2022-07-25 20:14:44 -0600

    effort

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,16 @@
+var location_id = [
+    // '3000035821',
+    // '3000022148',
+    // '3000016152',
+    // '3000020856',
+    '3000015474',
+    '3000035827',
+    '3000035823',
+    '3000035952',
+]
+
+// slc nyc buffalo austin rome stockholm
+
 var con = $(".contianer")
 
 const options = {
@@ -8,21 +21,37 @@ const options = {
     }
 };
 
-fetch('https://priceline-com-provider.p.rapidapi.com/v1/hotels/search?sort_order=HDR&location_id=3000035821&date_checkout=2022-11-16&date_checkin=2022-11-15&star_rating_ids=3.0%2C3.5%2C4.0%2C4.5%2C5.0&rooms_number=1&amenities_ids=FINTRNT%2CFBRKFST', options)
+for(var i= 0; i< location_id.length; i++){
+
+
+
+fetch('https://priceline-com-provider.p.rapidapi.com/v1/hotels/search?sort_order=HDR&location_id='+location_id[i]+'&date_checkout=2022-11-16&date_checkin=2022-11-15&star_rating_ids=3.0%2C3.5%2C4.0%2C4.5%2C5.0&rooms_number=1&amenities_ids=FINTRNT%2CFBRKFST', options)
     .then(response => response.json())
-    .then(response => console.log(response))
+    .then(priceline => {
+        fetch('https://covid-193.p.rapidapi.com/statistics', settings)
+            .then(response => response.json())
+            .then(response => {
+                    console.log(response)
+                    console.log(priceline)
+                    console.log(response.response.find(
+                        ({
+                            country
+                        }) => country === priceline.cityInfo.countryName));
+                    
+                }
+
+            )
+
+
+            .catch(err => console.error(err));
+    })
     .catch(err => console.error(err));
+}
 
-
-    const covid = {
-        method: 'GET',
-        headers: {
-            'X-RapidAPI-Key': 'aad7079f52msh020b1a143d31134p166daejsn76fc4309bafa',
-            'X-RapidAPI-Host': 'covid-193.p.rapidapi.com'
-        }
-    };
-    
-    fetch('https://covid-193.p.rapidapi.com/countries', covid)
-        .then(response => response.json())
-        .then(response => console.log(response))
-        .catch(err => console.error(err));
+const settings = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': '625324e95fmshe61bc7d687315bfp1b7c44jsn389593968b9d',
+        'X-RapidAPI-Host': 'covid-193.p.rapidapi.com'
+    }
+};
